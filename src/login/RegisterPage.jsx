8ec86301204c2ae56d0d8775871f263d3a6e59ee
Theerayuttu <theerayuttu@gmail.com>
2026-05-23 09:20:24 +0700

    feat: add keycode attribute to user registration
    - Add keyCodeRegis from server attributes in RegisterPage
    - Include keycode in registration request body attributes

--- src/login/RegisterPage.jsx
+++ src/login/RegisterPage.jsx
@@ -39,6 +39,7 @@ const RegisterPage = () => {
 
   const server = useSelector((state) => state.session.server);
   const totpForce = useSelector((state) => state.session.server.attributes.totpForce);
+  const keyCodeRegis = useSelector((state) => state.session.server.attributes.regiskey);
 
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
@@ -59,7 +60,9 @@ const RegisterPage = () => {
     await fetchOrThrow('/api/users', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ name, email, password, totpKey }),
+      body: JSON.stringify({ name, email, password, totpKey, attributes: {
+                              keycode: keyCodeRegis, 
+                            }, }),
     });
     setSnackbarOpen(true);
   });
